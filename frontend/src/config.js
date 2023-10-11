c39ca85367eea58961a3212f0128b79f7123fe79
abbasalitmk <abbasalitmk@gmail.com>
2023-10-11 14:36:49 +0530

    changes

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,15 @@
+// const config = {
+//   apiUrl: "http://localhost:8000/",
+//   baseUrl: "http://127.0.0.1:8000/",
+//   media_url: "http://127.0.0.1:8000",
+// };
+
+// export default config;
+
 const config = {
-  apiUrl: "http://localhost:8000/",
-  baseUrl: "http://127.0.0.1:8000/",
-  media_url: "http://127.0.0.1:8000",
+  apiUrl: "http://13.51.227.44/",
+  baseUrl: "http://13.51.227.44/",
+  media_url: "http://13.51.227.44/",
 };
 
 export default config;
