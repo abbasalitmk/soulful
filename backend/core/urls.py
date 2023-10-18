a374fbe3de2227d187862bb772b24a897d61ccc8
abbasalitmk <abbasalitmk@gmail.com>
2023-10-18 21:29:34 +0530

    changes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path("api/match/", include("match.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
